fd0bf5585a366a45e93f331aa9050fc4f6aa8a37
Thiago Ramos <thiagoramos.al@gmail.com>
2020-06-14 13:00:20 -0300

    Initial Commit

---
+++ MyPhotoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPhotoApp
+//
+//  Created by Thiago Ramos on 14/06/20.
+//  Copyright © 2020 Thiago Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
