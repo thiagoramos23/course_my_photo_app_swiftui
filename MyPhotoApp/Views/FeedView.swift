656b49bf3d7e8b62ffc5718dbbd5199a206f810d
Thiago Ramos <thiagoramos.al@gmail.com>
2020-06-16 01:17:50 -0300

    Almost finishing the animation to show the comments

--- MyPhotoApp/Views/FeedView.swift
+++ MyPhotoApp/Views/FeedView.swift
@@ -15,18 +15,19 @@ func screenSize() -> CGSize {
 struct Post: Identifiable {
     var id = UUID()
     var show: Bool
+    var imageName: String
 }
 
 let postData = [
-    Post(show: false),
-    Post(show: false),
-    Post(show: false),
-    Post(show: false),
-    Post(show: false)
+    Post(show: false, imageName: "friends"),
+    Post(show: false, imageName: "lake"),
+    Post(show: false, imageName: "show"),
+    Post(show: false, imageName: "trees"),
+    Post(show: false, imageName: "show")
 ]
 
 struct FeedView: View {
-    @State var posts: [Post] = postData
+    @State var posts: [Post] = postDataœ
     @State var show: Bool = false
     @State var activeIndex = -1
     
@@ -35,9 +36,9 @@ struct FeedView: View {
             VStack {
                 NavigationBarView(show: self.$show)
                 Spacer()
-                VStack {
+                GeometryReader { reader in
                     ScrollView {
-                        ForEach(posts.indices, id: \.self) { index in
+                        ForEach(self.posts.indices, id: \.self) { index in
                             GeometryReader { geometry in
                                 CardView(post: self.$posts[index], show: self.$show, activeIndex: self.$activeIndex, index: index)
                                     .offset(y: self.posts[index].show ? -geometry.frame(in: .global).minY + 40 : 0)
@@ -46,19 +47,31 @@ struct FeedView: View {
                             .zIndex(self.posts[index].show ? 10 : 0)
                             .offset(x: self.activeIndex != index && self.show ? screenSize().width : 0)
                         }
+                        
                     }
                     .offset(y: self.show ? -100 : 0)
+                    .frame(height: self.show ? screenSize().height : reader.size.height)
                     .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
                 }
             }
+            
         }
         .statusBar(hidden: self.show ? true : false)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
 struct FeedView_Previews: PreviewProvider {
     static var previews: some View {
-        FeedView()
+        Group {
+              FeedView()
+                 .previewDevice(PreviewDevice(rawValue: "iPhone XS"))
+                 .previewDisplayName("iPhone XS")
+
+              FeedView()
+                 .previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
+                 .previewDisplayName("iPhone XS Max")
+           }
     }
 }
 
@@ -112,46 +125,99 @@ struct CardView: View {
     @Binding var activeIndex: Int
     var index: Int
     
+    var closeButtonView: some View {
+        HStack {
+            Spacer()
+            Button(action: {
+                self.post.show.toggle()
+                self.show.toggle()
+            }) {
+                Image(systemName: "xmark.circle").font(.largeTitle)
+            }
+            .foregroundColor(Color.black)
+        }
+        .padding(.trailing, 8)
+    }
+    
+    var cardProfileView: some View {
+        HStack {
+            RoundedImageView()
+                .frame(width: 25, height: 25)
+                .padding(.leading)
+            VStack(alignment: .leading) {
+                Text("mille_f").font(.footnote).fontWeight(.bold)
+                HStack(alignment: .center) {
+                    Text("London, England").font(.footnote).foregroundColor(.secondary)
+                    Spacer()
+                    Text("2 minutes ago").font(.caption).foregroundColor(.secondary)
+                }
+            }
+            .padding(.trailing)
+        }
+    }
+    
+    var actionButtonsView: some View {
+        HStack(alignment: .center, spacing: 30) {
+            Button(action: {}) {
+                HStack {
+                    Image(systemName: "heart").font(Font.headline.weight(.semibold))
+                    Text("20").font(.caption)
+                }
+                
+            }.foregroundColor(.black)
+            Button(action: {}) {
+                HStack {
+                    Image(systemName: "bubble.right").font(Font.headline.weight(.semibold))
+                    Text("17").font(.caption)
+                }
+                
+            }.foregroundColor(.black)
+            Spacer()
+        }.padding()
+    }
+    
+    var commentsView: some View  {
+        VStack {
+            GeometryReader { reader in
+                ScrollView {
+                    ForEach(1...5, id: \.self) { index in
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image(uiImage: #imageLiteral(resourceName: "friends"))
+                                    .resizable()
+                                    .frame(width: 36, height: 36)
+                                    .clipShape(Circle())
+                                Text("mille_f").font(.footnote).fontWeight(.bold)
+                            }
+                            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. fdfdsklf;sddas jdklasjd askljd askljd askljd aklsdj aklsdj askldj askldjaskl djaskld ajskld asjkdla sjdklas djaskl djaskld asjkld asjkdl sajdkl sajdklas djaskl dj")
+                                .padding(.leading)
+                                .padding(.trailing)
+                        }
+                    }
+                }
+                .frame(height: self.show ? reader.size.height - 100 : reader.size.height)
+            }
+        }
+        .padding(.leading)
+        .padding(.trailing)
+    }
+    
     var body: some View {
         ZStack {
             VStack {
-                HStack {
-                    RoundedImageView()
-                        .frame(width: 25, height: 25)
-                        .padding(.leading)
-                    VStack(alignment: .leading) {
-                        Text("mille_f").font(.footnote).fontWeight(.bold)
-                        HStack(alignment: .center) {
-                            Text("London, England").font(.footnote).foregroundColor(.secondary)
-                            Spacer()
-                            Text("2 minutes ago").font(.caption).foregroundColor(.secondary)
-                        }
-                    }
-                    .padding(.trailing)
-                }
-                RoundedImageView(cornerRadius: 20)
+                show ? closeButtonView : nil
+                
+                cardProfileView
+                
+                RoundedImageView(image: UIImage(named: post.imageName)!, cornerRadius: 20)
                     .padding(.leading)
                     .padding(.trailing)
                     .shadow(color: Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)), radius: 15, x: 5, y: 10)
                     .frame(width: screenSize().width, height: 300)
+
+                actionButtonsView
                 
-                HStack(alignment: .center, spacing: 30) {
-                    Button(action: {}) {
-                        HStack {
-                            Image(systemName: "heart").font(Font.headline.weight(.semibold))
-                            Text("20").font(.caption)
-                        }
-                        
-                    }.foregroundColor(.black)
-                    Button(action: {}) {
-                        HStack {
-                            Image(systemName: "bubble.right").font(Font.headline.weight(.semibold))
-                            Text("17").font(.caption)
-                        }
-                        
-                    }.foregroundColor(.black)
-                    Spacer()
-                }.padding()
+                show ? commentsView : nil
             }
         }
         .onTapGesture {
@@ -163,6 +229,7 @@ struct CardView: View {
                 self.activeIndex = -1
             }
         }
+        .frame(height: show ? screenSize().height : 400)
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
     }
 }
